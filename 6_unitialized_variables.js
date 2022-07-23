67325d04f6c71d128162503df4d4a91f518d4289
fadhilmufid <mufidfadhil@gmail.com>
2022-07-23 11:46:32 +0700

    Try to add Unitialized String

---
+++ 6_unitialized_variables.js
@@ -0,0 +1,19 @@
+//Initialized these three variables
+
+//add 5
+var a = 5;
+//add 10
+var b = 10;
+//add Mupit nyoba nulis
+var c = "Mupit nyoba nulis";
+
+//Dont Change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+//testing result
+console.log(a)
+console.log(b)
+console.log(c)
