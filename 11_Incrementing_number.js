266a3c73d3767800e162f20fba192f1fc19912bc
fadhilmufid <mufidfadhil@gmail.com>
2022-07-23 12:06:31 +0700

    Incrementing number

---
+++ 11_Incrementing_number.js
@@ -0,0 +1,16 @@
+//Increment = adding variable by one
+
+//declare and assign variable
+var MyVar =87;
+
+//Increment
+//long code
+/*
+MyVar = MyVar + 1;
+*/
+
+//simplest solution
+MyVar++;
+
+//testing
+console.log(MyVar)
