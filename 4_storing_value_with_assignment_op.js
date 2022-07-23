d170874bffab5d28e27df96c50a9a0abc5fa0660
fadhilmufid <mufidfadhil@gmail.com>
2022-07-23 11:38:43 +0700

    4. Storing value with assignment

---
+++ 4_storing_value_with_assignment_op.js
@@ -0,0 +1,15 @@
+//declare variable a
+var a;
+//declare and assign variable b=2
+var b = 2;
+
+//a will be null because hasnt been assign 
+console.log(a);
+
+//asssign a
+a=7;
+//declare b to value of a
+b=a;
+
+//a = 7
+console.log(a);
