7bcceffdb5e7f6c913e2e5385dc23518f32f437b
fadhilmufid <mufidfadhil@gmail.com>
2022-07-23 12:25:28 +0700

    17. Compound Assignment Add

---
+++ 17_add_compound_assingment.js
@@ -0,0 +1,20 @@
+var a = 3;
+var b = 24;
+var c = 13;
+
+//the logic and long code
+/*
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+*/
+
+//short code
+a+=12;
+b+=9;
+c+=7;
+
+//testing
+console.log(a);
+console.log(b);
+console.log(c);
