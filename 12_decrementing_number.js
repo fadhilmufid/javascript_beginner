26d75ed61d50fb70f1316e486492de7c1da59ccc
fadhilmufid <mufidfadhil@gmail.com>
2022-07-23 12:09:01 +0700

    12. Decrementing Number

---
+++ 12_decrementing_number.js
@@ -0,0 +1,16 @@
+//Increment = substracting variable by one
+
+//declare and assign variable
+var MyVar =65;
+
+//Increment
+//long code
+/*
+MyVar = MyVar - 1;
+*/
+
+//simplest solution
+MyVar--;
+
+//testing
+console.log(MyVar)
