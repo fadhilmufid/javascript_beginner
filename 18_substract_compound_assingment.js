815dcb21dd3f3f1fb3547027e9fc03b22fd8c19e
fadhilmufid <mufidfadhil@gmail.com>
2022-07-23 12:27:50 +0700

    17. Compound Assignment Substract + Fix File Name

---
+++ 18_substract_compound_assingment.js
@@ -0,0 +1,20 @@
+var a = 3;
+var b = 24;
+var c = 13;
+
+//the logic and long code
+/*
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+*/
+
+//short code
+a-=2;
+b-=78;
+c-=56;
+
+//testing
+console.log(a);
+console.log(b);
+console.log(c);
