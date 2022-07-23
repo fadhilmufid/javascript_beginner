18f942d541fa9248a4fb98defa865c255992b7ab
fadhilmufid <mufidfadhil@gmail.com>
2022-07-23 12:34:09 +0700

    20. Compound Assignment Multply

--- 19_multiply_compound_assingment.js
+++ 19_multiply_compound_assingment.js
@@ -1,18 +1,18 @@
 var a = 4;
-var b = 93;
-var c = 11;
+var b = 144;
+var c = 72;
 
 //the logic and long code
 /*
-a = a * 3;
-b = 17 * b;
-c = c * 25;
+a = a/2;
+b = b/12;
+c = c/25;
 */
 
 //short code
-a*=3;
-b*=17;
-c*=25;
+a/=2;
+b/=12;
+c/=25;
 
 //testing
 console.log(a);
