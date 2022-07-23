cb0fd9e2e646be168fce0cb1ec69bd877b352347
fadhilmufid <mufidfadhil@gmail.com>
2022-07-23 11:33:26 +0700

    3. Data Types and Variable

---
+++ 3_datatypes_and_variables.js
@@ -0,0 +1,19 @@
+/*
+Data Types:
+undefined: something that hasnt been define like variable
+null: nothing
+boolean: true or false
+symbol: immutable primitive value taht uniqe
+number: number
+object: can store value
+*/
+
+//declare varable with var (able to be used to whole program)
+var myName = "Mufid"
+myName = 8
+
+//declare varable with let (only be used within scope where it declared)
+let ourName = "Fadhil"
+
+//declare varable with const (variable that cant never change, it wil be error if we try)
+const pi = 3.14
