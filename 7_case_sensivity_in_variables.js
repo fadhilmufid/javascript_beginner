57da405ae05e61ccb6d5d22b4fbb1ca9949f3ae0
fadhilmufid <mufidfadhil@gmail.com>
2022-07-23 11:55:32 +0700

    7. Case Sensitive In Javascript

---
+++ 7_case_sensivity_in_variables.js
@@ -0,0 +1,21 @@
+//Declaration
+/*Wrong
+var CoBaInDeh;
+var iniCamelCase;
+var IniTitleCase;
+*/
+
+/*Write Using Camel Case*/
+var cobainDeh; 
+var iniCamelCase;
+var iniTitleCase;
+
+//Assignment 
+cobainDeh = 2022; 
+iniCamelCase = "Fadhil";
+iniTitleCase = 19;
+
+//test
+console.log(cobainDeh)
+console.log(iniCamelCase)
+console.log(iniTitleCase)
