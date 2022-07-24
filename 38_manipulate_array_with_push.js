f5701549421a811097ddac640cdb04911ea6dfc5
fadhilmufid <mufidfadhil@gmail.com>
2022-07-24 09:12:53 +0700

    38. Manipulate Array with .Push

---
+++ 38_manipulate_array_with_push.js
@@ -0,0 +1,7 @@
+var ourArray = [["Muifd", 22, "UNS"],["Fikri", 20, "STAB"], ["Naila", 15, "SMP"]];
+
+//use ".push" to add another item isie the arrau
+ourArray.push(["Papa", 52, "Kemenkeu"]);
+
+//testing
+console.log(ourArray[3])
