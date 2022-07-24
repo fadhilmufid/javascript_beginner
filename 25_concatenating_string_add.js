5f23bb558c87a40b769abd4a67d26bee2e6014f8
fadhilmufid <mufidfadhil@gmail.com>
2022-07-24 08:10:28 +0700

    25 & 26 Concatenating and construct string

---
+++ 25_concatenating_string_add.js
@@ -0,0 +1,5 @@
+// we can use + to combine two string
+var outStr = "Tulisan Pertama. " + "Tulisan Kedua."
+
+//testing
+console.log(outStr)
---
+++ 26_construct_string_with_variable.js
@@ -0,0 +1,8 @@
+
+var firstName = "Fadhil"
+
+//you can add "firstName" to sentence variable in the midle of the string
+var sentence = "Hallo " + firstName + ", be strong"
+
+//testing
+console.log(sentence);
