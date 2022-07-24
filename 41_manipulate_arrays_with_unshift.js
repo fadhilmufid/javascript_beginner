609ebf666227a0d0b17f74279b3d812e6869139a
fadhilmufid <mufidfadhil@gmail.com>
2022-07-24 09:23:52 +0700

    41. Manipulate array with unshift to add item from beginning

---
+++ 41_manipulate_arrays_with_unshift.js
@@ -0,0 +1,9 @@
+var ourArray = [["Muifd", 22, "UNS"],["Fikri", 20, "STAN"], ["Naila", 15, "SMP"]];
+
+//use ".ushift" to add item to the first index in the array"
+ourArray.unshift("first item");
+
+//testing the array
+console.log(ourArray);
+
+//testing lastItem
