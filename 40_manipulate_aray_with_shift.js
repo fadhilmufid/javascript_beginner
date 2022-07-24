3a5d56a4605d60959bd6b81187b832bf408ba4dc
fadhilmufid <mufidfadhil@gmail.com>
2022-07-24 09:20:05 +0700

    Manipuate Array with shift to remove first item

---
+++ 40_manipulate_aray_with_shift.js
@@ -0,0 +1,10 @@
+var ourArray = [["Muifd", 22, "UNS"],["Fikri", 20, "STAN"], ["Naila", 15, "SMP"]];
+
+//use ".shift()" to remove first item inside the array"
+var firstItem = ourArray.shift();
+
+//testing the array
+console.log(ourArray);
+
+//testing lastItem
+console.log(firstItem);
