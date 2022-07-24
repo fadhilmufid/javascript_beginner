60711b6865368bf4e374f6d1bb5b6a7303465560
fadhilmufid <mufidfadhil@gmail.com>
2022-07-24 09:08:48 +0700

    37. Access in Nested Array with Index

---
+++ 37_access_item_in_multidimensional_array.js
@@ -0,0 +1,5 @@
+var ourArray = [["Muifd", 22, "UNS"],["Fikri", 20, "STAB"], ["Naila", 15, "SMP"]]
+
+//first bracket is for locating the index of the array inside, the secon bracket is for index number fo the item in the array 
+var namaNaila = ourArray[2][0];
+console.log(namaNaila);
