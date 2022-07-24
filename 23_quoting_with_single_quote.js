b270653a8c611f40d2ee2a5678f225520ae79238
fadhilmufid <mufidfadhil@gmail.com>
2022-07-24 07:56:53 +0700

    23. quoting with single and backtick

---
+++ 23_quoting_with_single_quote.js
@@ -0,0 +1,6 @@
+//contoh
+//we can use ' so the javascript will still recognize " as a sting
+var sentence = 'percobaan pake "single quote" di awal text'
+
+//we also can use `(back tick) because now it can recognize both ' and " as string
+var sentence = `tambahan kalimat di depan 'percobaan pake "single quote" di awal text'`
