fe3a60d6083e5c6939ed50ab2ca32f02a5b913b1
fadhilmufid <mufidfadhil@gmail.com>
2022-07-24 08:44:56 +0700

    31. Find Letter by length

---
+++ 31_find_letter_by_lenght.js
@@ -0,0 +1,9 @@
+//declare variable
+var lastLetter;
+var firstName = "Mufid";
+
+//use "[]" adn number from 0 to find the letter
+lastLetter = firstName[firstName.length - 1];
+
+//testing
+console.log(lastLetter);
