61f9ca632c47a45473f7b03822c42926898c7717
fadhilmufid <mufidfadhil@gmail.com>
2022-07-24 08:03:24 +0700

    24. Escape out string

---
+++ 24_escape_out_string.js
@@ -0,0 +1,17 @@
+/*
+Code Output
+\'  single quote
+\"  double quote
+\\  backlash
+\n  newline
+\r  carriage return
+\t  tab
+\b  backspace
+\f  form feed
+
+*/
+
+var myStr = "Line no 1\n\t\\Line no. 2\nThird Line";
+
+//testing
+console.log(myStr);
