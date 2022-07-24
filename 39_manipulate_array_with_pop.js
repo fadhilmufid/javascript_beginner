5b208e2fcc15e326a37842a8ce2d582103e5ed10
fadhilmufid <mufidfadhil@gmail.com>
2022-07-24 09:17:24 +0700

    Manipulate Array using pop to remove item

---
+++ 39_manipulate_array_with_pop.js
@@ -0,0 +1,10 @@
+var ourArray = [["Muifd", 22, "UNS"],["Fikri", 20, "STAN"], ["Naila", 15, "SMP"]];
+
+//use ".pop()" to remove last item inside the array"
+var lastItem = ourArray.pop();
+
+//testing the array
+console.log(ourArray);
+
+//testing lastItem
+console.log(lastItem);
